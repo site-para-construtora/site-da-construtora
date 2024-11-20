c3d46874abacf6ef73f9926cf8b6b854f4af9ded
Joa0CSilva <164250966+Joa0CSilva@users.noreply.github.com>
2024-11-20 15:14:09 -0300

    Update 20-11-2024
    Adicionei um novo carrossel na tela inicial

---
+++ site/script.js
@@ -0,0 +1,65 @@
+const galleryContainer = document.querySelector('.gallery-container');
+const galleryControlsContainer = document.querySelector('.gallery-controls');
+const galleryControls = ['previous', 'next'];
+const galleryItems = document.querySelectorAll('.gallery-item');
+const nextButton = document.getElementById('next-button');
+
+class Carousel {
+    constructor(container, items, controls) {
+        this.carouselConteiner = container;
+        this.carouselControls = controls;
+        this.carouselArray = [...items];
+    }
+
+    updateGallery() {
+        this.carouselArray.forEach(el => {
+            el.classList.remove('gallery-item-1');
+            el.classList.remove('gallery-item-2');
+            el.classList.remove('gallery-item-3');
+            el.classList.remove('gallery-item-4');
+            el.classList.remove('gallery-item-5');
+        });
+
+        this.carouselArray.slice(0, 5).forEach((el, i) => {
+            el.classList.add(`gallery-item-${i + 1}`);
+        });
+    }
+
+    setCurrentState(direction) {
+        if (direction === 'previous') {
+            this.carouselArray.unshift(this.carouselArray.pop());
+        } else {
+            this.carouselArray.push(this.carouselArray.shift());
+        }
+        this.updateGallery();
+    }
+
+    setControls() {
+        this.carouselControls.forEach(control => {
+            const button = document.createElement('button');
+            button.className = `gallery-controls-${control}`;
+            button.innerText = control === 'previous' ? 'Voltar' : 'Próximo'; // Define os textos personalizados
+            galleryControlsContainer.appendChild(button);
+        });
+    }
+
+    useControls() {
+        const triggers = [...galleryControlsContainer.childNodes];
+        triggers.forEach(control => {
+            control.addEventListener('click', e => {
+                e.preventDefault();
+                this.setCurrentState(control.className.includes('previous') ? 'previous' : 'next');
+            });
+        });
+    }
+}
+
+const exampleCarousel = new Carousel(galleryContainer, galleryItems, galleryControls);
+
+exampleCarousel.setControls();
+exampleCarousel.useControls();
+
+// Evento para o botão "Próxima Imagem"
+nextButton.addEventListener('click', () => {
+    exampleCarousel.setCurrentState('next');
+});
